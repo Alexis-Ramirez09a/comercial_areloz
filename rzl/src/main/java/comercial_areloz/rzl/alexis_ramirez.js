496f354ec96ea6f65308077161c7806faa58e573
Alexis Ramirez <alexis0991768994@gmail.com>
2025-10-02 19:13:19 -0500

    Creación de Saludo para invitado

--- rzl/src/main/java/comercial_areloz/rzl/alexis_ramirez.js
+++ rzl/src/main/java/comercial_areloz/rzl/alexis_ramirez.js
@@ -2,3 +2,7 @@ function Hello() {
     var mensaje="Hola Mundo";
     console.log(mensaje);
 }
+function Saludo(invitado) {
+    var mensaje="Hola, bienvenido"+invitado;
+    console.log(mensaje);
+}
